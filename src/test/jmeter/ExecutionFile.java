b9a97db79c747e03965ee34136881bd1145e78ac
kannanram95 <kannanram1995@gmail.com>
2025-02-19 11:57:42 +0530

    Config.json added, Updated ExecutionFile and deleted SampleJmeter file

--- src/test/jmeter/ExecutionFile.java
+++ src/test/jmeter/ExecutionFile.java
@@ -3,55 +3,58 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jorphan.collections.ListedHashTree;
 import org.base.Utils;
-import org.testng.annotations.Test;
-
 import java.io.*;
-public class ExecutionFile {
 
+public class ExecutionFile {
     public static void main(String[] argv) throws Exception {
 
         //load properties
         Utils utils = new Utils();
-        String payloadPath = Utils.loadProperties("payloadFile");
-        int tps = Integer.parseInt(Utils.loadProperties("tps"));
-        int rampUpTimeSec = Integer.parseInt(Utils.loadProperties("rampUp"));
-        int duration = Integer.parseInt(Utils.loadProperties("duration"));
-        double apiResponseTime = Double.parseDouble(Utils.loadProperties("apiResponseTime"));
+        String payload = Utils.loadProperties("payloadFile");
+        String api = Utils.loadProperties("apiName");
+        System.out.println("API " +api);
+        String variableName = Utils.loadProperties("dynamic_value_variables");
+        System.out.println(variableName);
+        String[] variables = variableName.split(",");
+
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readTree(new File(payloadPath));
+        JsonNode rootNode = mapper.readTree(new File(payload));
         //Start Jmeter Engine
         utils.initJmeter();
         String testName = rootNode.get("info").get("name").asText();
 
-        //Creating hash tree
-        ListedHashTree ListedHashTree= new ListedHashTree();
-        ListedHashTree testPlan = utils.testPlan(testName, ListedHashTree);
-        ListedHashTree threadGroup = utils.threadGroup("ThreadGroup",testPlan,tps,rampUpTimeSec,duration, apiResponseTime,-1);
-
-            // sampler
+        ListedHashTree hashTree= new ListedHashTree();
+        ListedHashTree testPlan = utils.testPlan(testName, hashTree);
+        ListedHashTree threadGroup = utils.threadGroup("ThreadGroup",testPlan,20,1,30,1);
+        if(api.equalsIgnoreCase("null")){
+            String filePath = "csvFiles/Global_config.csv";
+            utils.csvDataConfig(testPlan,filePath,variables);
+        }
         JsonNode items = rootNode.get("item");
         for (JsonNode item : items) {
             String apiMethod = item.get("request").get("method").asText();
-            ListedHashTree sampler = utils.httpSampler(item,threadGroup);
-            if(!item.get("request").get("header").isNull()){
+            String apiName = item.get("name").asText();
+            ListedHashTree sampler = utils.httpSampler(item, threadGroup);
+            if (!item.get("request").get("header").isNull()) {
                 JsonNode header = item.get("request").get("header");
-                utils.headerManager(sampler,header);
+                utils.headerManager(sampler, header);
             }
-            if(apiMethod.equalsIgnoreCase("POST")){
-                utils.responseAssertion("200",sampler);
-//                utils.csvDataSet(sampler);
-            }else {
-                utils.responseAssertion("200",sampler);
+            if(apiName.equalsIgnoreCase(api)){
+                String filePath = "csvFiles/"+apiName+"config.csv";
+                utils.csvDataConfig(sampler,filePath,variables);
             }
-
+            if (apiMethod.equalsIgnoreCase("POST")) {
+                utils.responseAssertion("201", sampler);
+            } else {
+                utils.responseAssertion("200", sampler);
+            }
         }
-
         // Save Test Plan to JMX File
         String jmxFile = utils.JMXFileCreator(testName);
         try (FileOutputStream fos = new FileOutputStream(jmxFile)) {
-            SaveService.saveTree(ListedHashTree, fos);
+            SaveService.saveTree(hashTree, fos);
         }
         //Command-Line execution
-        utils.runJmxFile(jmxFile);
+        //utils.runJmxFile(jmxFile);
     }
 }
